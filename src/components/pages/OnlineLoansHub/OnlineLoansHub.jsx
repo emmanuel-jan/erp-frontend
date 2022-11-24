6952a937fca28bcdf354774a9ab219d83241dc1f
emmanuel-jan <emmanueljan80@gmail.com>
2022-11-24 20:02:02 +0300

    update the table section

--- src/components/pages/OnlineLoansHub/OnlineLoansHub.jsx
+++ src/components/pages/OnlineLoansHub/OnlineLoansHub.jsx
@@ -15,6 +15,9 @@ import {
   Tag,
   Table,
   Input,
+  Form,
+  Radio,
+  Switch,
 } from "antd";
 import { SearchOutlined, FilePdfOutlined } from "@ant-design/icons";
 import Highlighter from "react-highlight-words";
@@ -32,16 +35,97 @@ for (let i = 0; i < 46; i++) {
   data.push({
     key: i,
     name: `Edward King ${i}`,
-    age: 32,
+    age: 32 + i,
     address: `London, Park Lane no. ${i}`,
   });
 }
+
+const defaultExpandable = {
+  expandedRowRender: (record) => <p>{record.description}</p>,
+};
+const defaultTitle = () => "Here is title";
+const defaultFooter = () => "Here is footer";
+
 const OnlineLoansHub = (props) => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [searchedColumn, setSearchedColumn] = useState("");
   const searchInput = useRef(null);
   const componentRef = useRef();
+  const [bordered, setBordered] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [size, setSize] = useState("large");
+  const [expandable, setExpandable] = useState(defaultExpandable);
+  const [showTitle, setShowTitle] = useState(false);
+  const [showHeader, setShowHeader] = useState(true);
+  const [showfooter, setShowFooter] = useState(true);
+  const [rowSelections, setRowSelections] = useState({});
+  const [hasData, setHasData] = useState(true);
+  const [tableLayout, setTableLayout] = useState(undefined);
+  const [top, setTop] = useState("none");
+  const [bottom, setBottom] = useState("bottomRight");
+  const [ellipsis, setEllipsis] = useState(false);
+  const [yScroll, setYScroll] = useState(false);
+  const [xScroll, setXScroll] = useState(undefined);
+  const handleBorderChange = (enable) => {
+    setBordered(enable);
+  };
+  const handleLoadingChange = (enable) => {
+    setLoading(enable);
+  };
+  const handleSizeChange = (e) => {
+    setSize(e.target.value);
+  };
+  const handleTableLayoutChange = (e) => {
+    setTableLayout(e.target.value);
+  };
+  const handleExpandChange = (enable) => {
+    setExpandable(enable ? defaultExpandable : undefined);
+  };
+  const handleEllipsisChange = (enable) => {
+    setEllipsis(enable);
+  };
+  const handleTitleChange = (enable) => {
+    setShowTitle(enable);
+  };
+  const handleHeaderChange = (enable) => {
+    setShowHeader(enable);
+  };
+  const handleFooterChange = (enable) => {
+    setShowFooter(enable);
+  };
+  const handleRowSelectionChange = (enable) => {
+    setRowSelections(enable ? {} : undefined);
+  };
+  const handleYScrollChange = (enable) => {
+    setYScroll(enable);
+  };
+  const handleXScrollChange = (e) => {
+    setXScroll(e.target.value);
+  };
+  const handleDataChange = (newHasData) => {
+    setHasData(newHasData);
+  };
+  const scroll = {};
+  if (yScroll) {
+    scroll.y = 240;
+  }
+  if (xScroll) {
+    scroll.x = "100vw";
+  }
+
+  const tableProps = {
+    bordered,
+    loading,
+    size,
+    expandable,
+    title: showTitle ? defaultTitle : undefined,
+    showHeader,
+    footer: showfooter ? defaultFooter : undefined,
+    rowSelections,
+    scroll,
+    tableLayout,
+  };
 
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
@@ -216,6 +300,7 @@ const OnlineLoansHub = (props) => {
       field: "age",
       header: "age",
       ...getColumnSearchProps("age"),
+      sorter: (a, b) => a.age - b.age,
     },
     {
       title: "Address",
@@ -229,20 +314,29 @@ const OnlineLoansHub = (props) => {
     },
   ];
 
+  const tableColumns = columns.map((item) => ({
+    ...item,
+    ellipsis,
+  }));
+  if (xScroll === "fixed") {
+    tableColumns[0].fixed = true;
+    tableColumns[tableColumns.length - 1].fixed = "right";
+  }
+
   const exportColumns = columns.map((col) => ({
     title: col.header,
-    dataKey: col.field
-  }))
+    dataKey: col.field,
+  }));
 
   const exportPDF = () => {
     import("jspdf").then((jsPDF) => {
       import("jspdf-autotable").then(() => {
-        const doc = new jsPDF.default(0,0);
+        const doc = new jsPDF.default(0, 0);
         doc.autoTable(exportColumns, data);
-        doc.save("pending_applications.pdf")
-      })
-    } )
-  }
+        doc.save("pending_applications.pdf");
+      });
+    });
+  };
 
   return (
     <>
@@ -259,8 +353,28 @@ const OnlineLoansHub = (props) => {
           minHeight: 280,
         }}
       >
-        <Button type="primary">Loan Calculator</Button>
-        <Button>Apply for a Loan</Button>
+        <Space style={{ float: "right" }}>
+          <Button type="primary">Loan Calculator</Button>
+          <Button>Apply for a Loan</Button>
+        </Space>
+
+        <Space>
+          <Button type="primary">
+            <CSVLink
+              filename={"Pending_Applications.csv"}
+              data={data}
+              className="btn btn-primary"
+              onClick={() => {
+                console.log("The file is downloading");
+              }}
+            >
+              Export to CSV
+            </CSVLink>
+          </Button>
+          <Button onClick={exportPDF} type="primary" danger>
+            <FilePdfOutlined /> Export to PDF{" "}
+          </Button>
+        </Space>
         <Tabs
           defaultActiveKey="1"
           onChange={onChange}
@@ -270,28 +384,131 @@ const OnlineLoansHub = (props) => {
               key: "1",
               children: (
                 <>
-                  <Space style={{ float: "right" }}>
-                    <Button type="primary">
-                      <CSVLink
-                        filename={"Pending_Applications.csv"}
-                        data={data}
-                        className="btn btn-primary"
-                        onClick={() => {
-                          console.log("The file is downloading");
+                  <Form
+                    layout="inline"
+                    className="components-table-demo-control-bar"
+                    style={{
+                      marginBottom: 16,
+                    }}
+                  >
+                    <Form.Item label="Bordered">
+                      <Switch
+                        checked={bordered}
+                        onChange={handleBorderChange}
+                      />
+                    </Form.Item>
+                    <Form.Item label="loading">
+                      <Switch
+                        checked={loading}
+                        onChange={handleLoadingChange}
+                      />
+                    </Form.Item>
+                    <Form.Item label="Title">
+                      <Switch
+                        checked={showTitle}
+                        onChange={handleTitleChange}
+                      />
+                    </Form.Item>
+                    <Form.Item label="Column Header">
+                      <Switch
+                        checked={showHeader}
+                        onChange={handleHeaderChange}
+                      />
+                    </Form.Item>
+                    <Form.Item label="Footer">
+                      <Switch
+                        checked={showfooter}
+                        onChange={handleFooterChange}
+                      />
+                    </Form.Item>
+                    <Form.Item label="Expandable">
+                      <Switch
+                        checked={!!expandable}
+                        onChange={handleExpandChange}
+                      />
+                    </Form.Item>
+                    {/* <Form.Item label="Checkbox">
+          <Switch checked={!!rowSelections} onChange={handleRowSelectionChange} />
+        </Form.Item> */}
+                    <Form.Item label="Fixed Header">
+                      <Switch
+                        checked={!!yScroll}
+                        onChange={handleYScrollChange}
+                      />
+                    </Form.Item>
+                    <Form.Item label="Has Data">
+                      <Switch checked={!!hasData} onChange={handleDataChange} />
+                    </Form.Item>
+                    {/* <Form.Item label="Ellipsis">
+          <Switch checked={!!ellipsis} onChange={handleEllipsisChange} />
+        </Form.Item> */}
+                    <Form.Item label="Size">
+                      <Radio.Group value={size} onChange={handleSizeChange}>
+                        <Radio.Button value="large">Large</Radio.Button>
+                        <Radio.Button value="middle">Middle</Radio.Button>
+                        <Radio.Button value="small">Small</Radio.Button>
+                      </Radio.Group>
+                    </Form.Item>
+                    <Form.Item label="Table Scroll">
+                      <Radio.Group
+                        value={xScroll}
+                        onChange={handleXScrollChange}
+                      >
+                        <Radio.Button value={undefined}>Unset</Radio.Button>
+                        <Radio.Button value="scroll">Scroll</Radio.Button>
+                        <Radio.Button value="fixed">Fixed Columns</Radio.Button>
+                      </Radio.Group>
+                    </Form.Item>
+                    <Form.Item label="Table Layout">
+                      <Radio.Group
+                        value={tableLayout}
+                        onChange={handleTableLayoutChange}
+                      >
+                        <Radio.Button value={undefined}>Unset</Radio.Button>
+                        <Radio.Button value="fixed">Fixed</Radio.Button>
+                      </Radio.Group>
+                    </Form.Item>
+                    <Form.Item label="Pagination Top">
+                      <Radio.Group
+                        value={top}
+                        onChange={(e) => {
+                          setTop(e.target.value);
                         }}
                       >
-                        Export to CSV
-                      </CSVLink>
-                    </Button>
-                    <Button onClick={exportPDF} type="primary" danger>
-                      <FilePdfOutlined /> Export to PDF{" "}
-                    </Button>
-                  </Space>
+                        <Radio.Button value="topLeft">TopLeft</Radio.Button>
+                        <Radio.Button value="topCenter">TopCenter</Radio.Button>
+                        <Radio.Button value="topRight">TopRight</Radio.Button>
+                        <Radio.Button value="none">None</Radio.Button>
+                      </Radio.Group>
+                    </Form.Item>
+                    <Form.Item label="Pagination Bottom">
+                      <Radio.Group
+                        value={bottom}
+                        onChange={(e) => {
+                          setBottom(e.target.value);
+                        }}
+                      >
+                        <Radio.Button value="bottomLeft">
+                          BottomLeft
+                        </Radio.Button>
+                        <Radio.Button value="bottomCenter">
+                          BottomCenter
+                        </Radio.Button>
+                        <Radio.Button value="bottomRight">
+                          BottomRight
+                        </Radio.Button>
+                        <Radio.Button value="none">None</Radio.Button>
+                      </Radio.Group>
+                    </Form.Item>
+                    <Form.Item></Form.Item>
+                  </Form>
                   <div ref={componentRef}>
                     <Table
+                      {...tableProps}
                       rowSelection={rowSelection}
-                      columns={columns}
-                      dataSource={data}
+                      columns={tableColumns}
+                      dataSource={hasData ? data : []}
+                      scroll={scroll}
                     />
                   </div>
                 </>
